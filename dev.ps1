04cac230e4e8f504abed679a2d9f8182c53cee2c
apteryxxyz <apteryx.xyz@gmail.com>
2025-06-09 22:08:40 +1200

    Add additional functionality to dev script

--- dev.ps1
+++ dev.ps1
@@ -1,25 +1,69 @@
-Add-Type -AssemblyName System.Windows.Forms
+$STEAM = "C:\Program Files (x86)\Steam"
 
-#
+function Stop-Steam {
+  Write-Output "Checking for existing Steam process..."
+  $steamProcess = Get-Process -Name "steam" -ErrorAction SilentlyContinue
 
-Write-Output "Checking for existing Steam process..."
-$steamProcess = Get-Process -Name "steam" -ErrorAction SilentlyContinue
-
-if ($steamProcess) {
-  Write-Output "Steam is running. Stopping process..."
-  Stop-Process -Name "steam" -Force
+  if ($steamProcess) {
+    Write-Output "Steam is running, stopping process..."
+    Stop-Process -Name "steam" -Force
+  }
+  else {
+    Write-Output "Steam is not running, skipping..."
+  }
 }
-else {
-  Write-Output "Steam is not running."
+
+function Build-Plugin {
+  Write-Output "Building plugin..."
+  pnpm millennium-ttc --build dev
 }
 
-#
+function Create-Plugin-Symlink {
+  $pluginName = (Get-Content "./plugin.json" | ConvertFrom-Json).name
+  $targetPath = "$STEAM\plugins\$pluginName"
+  $sourcePath = Get-Location
 
-Write-Output "Building plugin..."
-pnpm millennium-ttc --build dev
+  if (!(Test-Path $targetPath)) {
+    Write-Output "Creating symlink to plugins directory..."
+    New-Item -Path $targetPath -ItemType SymbolicLink -Value $sourcePath | Out-Null
+  }
+  else {
+    Write-Output "Plugin symlink already exists, skipping..."
+  }
+}
 
-#
+function Enable-Plugin {
+  $pluginName = (Get-Content "./plugin.json" | ConvertFrom-Json).name
+  $millenniumPath = "$STEAM\ext\millennium.ini"
+  $iniContent = Get-Content $millenniumPath
+  
+  if ($iniContent | Select-String -Pattern $pluginName) {
+    Write-Output "Plugin already enabled, skipping..."
+  }
+  else {
+    Write-Output "Enabling plugin..."
+    $iniContent = $iniContent -replace "core", "core|$pluginName"
+    $iniContent | Set-Content $millenniumPath
+  }
+}
 
-Write-Output "Starting Steam in dev mode..."
-Start-Process -FilePath "C:\Program Files (x86)\Steam\steam.exe" -ArgumentList "-dev"
-Start-Sleep -Seconds 10
+function Start-SteamDevMode {
+  Write-Output "Starting Steam in dev mode..."
+  Start-Process -FilePath "$STEAM\steam.exe" -ArgumentList "-dev"
+  Start-Sleep -Seconds 10
+}
+
+function Open-DevTools {
+  Add-Type -AssemblyName System.Windows.Forms
+  Write-Output "Pressing F12 to open DevTools..."
+  [System.Windows.Forms.SendKeys]::SendWait("{F12}")
+}
+
+# ===== Main Execution ===== #
+
+Stop-Steam
+Build-Plugin
+Create-Plugin-Symlink
+Enable-Plugin
+Start-SteamDevMode
+Open-DevTools
