d6eee1d0d04ba1c01bf5b1a06edd2406c5798a7e
Apteryx <apteryx.xyz@gmail.com>
2025-08-08 05:08:35 +1200

    Fix location not being monitoring after exiting big picture mode

--- frontend/monitors/location.ts
+++ frontend/monitors/location.ts
@@ -1,22 +1,33 @@
 import { waitFor } from '../helpers.js';
 import Steam from '../steam.js';
 
+// In previous versions of this, it just added a listener on history, but
+// for whatever reason, when exiting Big Picture mode, the listener would
+// be removed
+
+const MONITOR_LOCATION_POLL_INTERVAL = 500;
+
 /**
  * Monitor the location of the Steam window
  * @param options Options for the monitor
  */
 export async function monitorLocation({
   onChange,
+  signal,
 }: {
   onChange?: (location: Steam.MainWindowBrowserLocation) => void;
+  signal?: AbortSignal;
 }) {
   await waitFor(() => Steam.MainWindowBrowserManager);
-  const history = Steam.MainWindowBrowserManager.m_history;
 
   let lastLocation = { pathname: '' };
-  history.listen((location) => {
+
+  const monitor = setInterval(() => {
+    if (signal?.aborted) return clearInterval(monitor);
+
+    const location = Steam.MainWindowBrowserManager.m_lastLocation;
     if (lastLocation.pathname === location.pathname) return;
     lastLocation = location;
     onChange?.(location);
-  });
+  }, MONITOR_LOCATION_POLL_INTERVAL);
 }
