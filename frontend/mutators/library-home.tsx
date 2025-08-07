cd852db39e7e9fc7d5e5350f97ce8262ab824ebc
Apteryx <apteryx.xyz@gmail.com>
2025-08-08 06:01:12 +1200

    Update missed size_on_disk usage

--- frontend/mutators/library-home.tsx
+++ frontend/mutators/library-home.tsx
@@ -1,13 +1,14 @@
 import { beforePatch } from '@steambrew/client';
 import rpc from '../rpc.js';
 import Steam from '../steam.js';
+import { NON_STEAM_APP_APPID_MASK } from '../constants.js';
 
 beforePatch(
   Steam.CollectionStore,
   'OnAppOverviewChange',
   // @ts-ignore
   async function (this: Steam.CollectionStore, [apps]: [Steam.AppOverview[]]) {
-    const nonSteamApps = apps.filter((a) => a.size_on_disk === '0');
+    const nonSteamApps = apps.filter((a) => a.appid > NON_STEAM_APP_APPID_MASK);
     const playTimings = await rpc.GetPlaytimes(
       nonSteamApps.map((a) => a.display_name),
     );
